b01d9b3d9548d538e318e6f33861e6f502724d0a
Jonathan Roddy <49447734+Jonathan-Roddy@users.noreply.github.com>
2022-02-05 00:11:14 +0000

    First commit
    Updating the repo with code

---
+++ rules.mk
@@ -0,0 +1,24 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+
+BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes            # USB Nkey Rollover
+TAP_DANCE_ENABLE = no
+ENCODER_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+EXTRAFLAGS += -flto
